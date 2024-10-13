7ccd2fbffd960d201e8dab2872221aff5f802e7f
Alejo141 <66750054+Alejo141@users.noreply.github.com>
2024-10-13 17:12:24 -0500

    Add files via upload

--- Creg_166.py
+++ Creg_166.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import openpyxl as op
-#import pip
-#pip.main(["install", "openpyxl"])
+import pip
+pip.main(["install", "openpyxl"])
 
 st.title("Cálculos CREG 166")
 
